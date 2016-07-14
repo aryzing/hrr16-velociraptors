07d714d34d34ea507281ba1feb7aeacd86cd4fea
Eduard Bardají Puig <ebardaji@gmail.com>
2016-07-13 17:07:44 -0700

    closes #175

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var app = require('./server/server.js');
 var mongoose = require('mongoose');
 
-mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/greenfield';
+var mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/greenfield';
 mongoose.connect(mongoURI);
 
-port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Server started on port ', port);
